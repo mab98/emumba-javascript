bdc537680d142b5c9633418811223c89c3f5e384
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-03-03 16:40:18 +0500

    applied ternary operator in phoneNumberValidator

--- javascript-Projects/phoneNumberValidator.js
+++ javascript-Projects/phoneNumberValidator.js
@@ -1,10 +1,9 @@
 function telephoneCheck (str) {
-  if (
-    str.match(/^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/)
-  ) {
-    return true
-  }
-  return false
+  return str.match(
+    /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/
+  )
+    ? true
+    : false
 }
 
 console.log(telephoneCheck('1 (555) 555-5555'))
